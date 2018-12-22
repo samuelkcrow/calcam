68920694293e506fa7ee0a2ec74982774fac6132
samuelkcrow <samuelkcrow@gmail.com>
2018-12-22 06:34:51 +0000

    Changes to how image files are stored and threads to run grabber and giffer

--- c.py
+++ c.py
@@ -1,48 +1,59 @@
+#!/usr/bin/python3
+
 import requests
 import shutil
 import time
 import threading 
 import os
+import imageio
+import sys
 
 
 def getImage():
+    global image_count
     url = "https://www.calvin.edu/img/calcam_large.jpg"
     response = requests.get(url, stream=True)
-    current_time = time.strftime("%b%d-%H%M",time.gmtime())
-    image_file = "img/calcam_large" + current_time + ".jpg"
+    current_time = time.strftime("%b%d-%H%M", time.gmtime())
+    image_directory = os.path.join(os.path.dirname(sys.argv[0]), 'img')
+    image_file = os.path.join(image_directory, 'calcam_large' + current_time + ".jpg")
     with open(image_file, 'wb') as out_file:
         shutil.copyfileobj(response.raw, out_file)
     del response
+    image_count += 1
     print("Image retrieved")
 
-def waitUntil(period=1):
-    '''Waits until next minute
+def doEvery(period, methodToCall):
+    '''Calls a method every period
+    Input: methodToCall, method
     '''
-    end_time = (time.time() // 60 + 1) * 60
-    while time.time() < end_time:
-        time.sleep(period)
-    return False
+    while True:
+        end_time = (time.time() // period + period / 60) * 60
+        while time.time() < end_time:
+            time.sleep(period / 60)
+        methodToCall()
 
 def startThread(threadTarget, *args, **kwargs):
+    '''Runs a method in a separate thread
+    Input: threadTarget, the method being started
+    '''
     thread = threading.Thread(target=threadTarget, args=args, kwargs=kwargs)
     thread.start()
     print("Thread is waiting in background")
 
 def makeGif():
-    jpg_directory = '/img'
+    jpg_directory = 'img'
     images = []
     for file_name in os.listdir(jpg_directory):
         if file_name.endswith('.jpg'):
-            file_path = os.path.join(jpg_directory, file_name)
-            images.append(imageio.imread(file_path))
-    imageio.mimsave('/var/www/calcam.gif', images)
+           file_path = os.path.join(jpg_directory, file_name)
+           images.append(imageio.imread(file_path))
+    imageio.mimsave('/var/www/html/img/calcam.gif', images)
 
-def imageLoop():
-    count = 0
-    while count < 4:
-        getImage()
-        startThread(waitUntil)
-        count += 1
+#------------------ Main ----------------------#
 
-imageLoop()
+image_count = 0
+getImage()
+startThread(doEvery, 60, getImage)
+startThread(doEvery, 3600, makeGif)
+
 
