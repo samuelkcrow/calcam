001c4047cc0c4785e8ad1146dca17faddeef68a1
samuelkcrow <samuelkcrow@gmail.com>
2018-12-23 08:01:06 +0000

    fixed thread timing module and image naming

--- c.py
+++ c.py
@@ -9,17 +9,16 @@ import imageio
 import sys
 
 
+image_directory = os.path.join(os.path.dirname(sys.argv[0]), 'commons_lawn')
+url = "https://www.calvin.edu/img/calcam_large.jpg"
+
 def getImage():
     global image_count
-    url = "https://www.calvin.edu/img/calcam_large.jpg"
     response = requests.get(url, stream=True)
-    current_time = time.strftime("%b%d-%H%M", time.gmtime())
-    image_directory = os.path.join(os.path.dirname(sys.argv[0]), 'img')
-    image_file = os.path.join(image_directory, 'calcam_large' + current_time + ".jpg")
+    image_file = os.path.join(image_directory, str(int(time.time())) + ".jpg")
     with open(image_file, 'wb') as out_file:
         shutil.copyfileobj(response.raw, out_file)
     del response
-    image_count += 1
     print("Image retrieved")
 
 def doEvery(period, methodToCall):
@@ -28,7 +27,7 @@ def doEvery(period, methodToCall):
     '''
     try:
         while True:
-            end_time = (time.time() // period + period / 60) * period
+            end_time = (time.time() // period + 1) * period
             while time.time() < end_time:
                 time.sleep(period / 60)
             methodToCall()
@@ -45,22 +44,22 @@ def startThread(threadTarget, *args, **kwargs):
     print("Thread is waiting in background")
 
 def makeGif():
-    jpg_directory = 'img'
     images = []
-    for file_name in os.listdir(jpg_directory):
+    for file_name in sorted(os.listdir(image_directory)):
+        print(file_name)
         if file_name.endswith('.jpg'):
-           file_path = os.path.join(jpg_directory, file_name)
+           file_path = os.path.join(image_directory, file_name)
            images.append(imageio.imread(file_path))
     imageio.mimsave('/var/www/html/img/calcam.gif', images)
     print("Gif made")
 
 #------------------ Main ----------------------#
 
-image_count = 0
 getImage()
+makeGif()
 startThread(doEvery, 60, getImage)
-startThread(doEvery, 3600, makeGif)
-print("Hey maybe we don't have to exit gracefully if we don't want to")
+startThread(doEvery, 300, makeGif)
+print("Waiting...")
 while True:
     time.sleep(10)
 
