3a3a7ee8974b7f3b0f667b5785a0f0087b1250d8
samuelkcrow <samuelkcrow@gmail.com>
2019-01-13 19:08:22 +0000

    New date and duration call for gif

--- c.py
+++ c.py
@@ -40,44 +40,50 @@ class Camera:
         logging.debug("Thread is waiting in background")
 
 
-    def makeGif(self, start_time, end_time, frames_per_second=20, frames_per_hour=20):
+    def makeGif(self, start_time, duration, frames_per_second=20, frames_per_hour=20):
         '''Make a gif from a range of images
-        start and end: time stamps in seconds
+        Start_time: time in y-m-d h:m:s format
+        Duration: in hours
         '''
         images = []
         gif_path = os.path.join(self.dir, 'gif', 'calcam' + '.gif')
         image_list = os.listdir(os.path.join(self.dir, 'img'))
         image_list.sort()
+        start_time = int(time.mktime(time.strptime(start_time, "%Y-%m-%d %H:%M:%S"))) + 5 * 3600  # timezone?
+        logging.debug(start_time)
+        end_time = start_time + 3600 * duration
         start_image = str(start_time) + '.jpg'
         end_image = str(end_time) + '.jpg'
         logging.debug("Scanning images...")
-        for index in range(len(image_list)):
-            if start_image <= image_list[index] and image_list[index].endswith('.jpg'):
-                start_index = index
+        for image in image_list:
+            if start_image <= image and image.endswith('.jpg'):
+                start_image = image
+                start_index = image_list.index(image)
                 break
             else:
                 continue
             logging.debug("Invalid start time, gif creation aborted...")
             return
-        for index in range(len(image_list)):
-            reverse_image_list = sorted(image_list, reverse=True)
-            if end_image >= reverse_image_list[index] and reverse_image_list[index].endswith('.jpg'):
-                end_index = len(image_list) - index - 1
+        reverse_image_list = sorted(image_list, reverse=True)
+        for image in reverse_image_list:
+            if end_image >= image and image.endswith('.jpg'):
+                end_image = image
+                end_index = image_list.index(image)
                 break
             else:
                 continue
             logging.debug("Invalid end time, gif creation aborted...")
             return
-        logging.debug("Creating gif...")
         # actual start and end times
-        start_time = int(image_list[start_index][:-4])
-        end_time = int(image_list[end_index][:-4])
+        start_time = int(start_image[:-4])
+        end_time = int(end_image[:-4])
         actual_frames_per_hour = len(image_list) / (end_time - start_time)
         frame_multiplier = actual_frames_per_hour // frames_per_hour + 1
         for index in range(start_index, end_index + 1):
             if index % frame_multiplier == 0:
                 file_path = os.path.join(self.dir, 'img', image_list[index])
                 images.append(imageio.imread(file_path))
+        logging.debug("Creating gif...")
         imageio.mimsave(gif_path, images, fps=frames_per_second)
         logging.debug("Gif completed")
 
@@ -92,4 +98,4 @@ while True:
     except KeyboardInterrupt:
         break
 '''
-calcam.makeGif(1546550000,1546580000,20,10)
+calcam.makeGif('2019-01-04 16:00:00',8,20,10)
